2d66c08da41849724793b015a48b16fa6002af0b
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-03-28 03:15:08 +0200

    Update app.ts

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,3 +1,5 @@
-import { initializeApp } from "firebase-admin/app";
+import { initializeApp, applicationDefault } from "firebase-admin/app";
 
-export const app = initializeApp();
+export const app = initializeApp({
+  credential: applicationDefault(),
+});
