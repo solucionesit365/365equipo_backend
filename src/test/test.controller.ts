7d880938eb2e7688f5344dc2176cb30b9dc23e7d
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-03-28 14:06:57 +0200

    Test axios

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,9 +1,21 @@
 import { Controller, Get } from "@nestjs/common";
+import axios from "axios";
 
 @Controller("test")
 export class TestController {
   @Get()
   test() {
     return "este es el primer test";
+  }
+
+  @Get("ip")
+  async getIp(): Promise<string> {
+    try {
+      const response = await axios.get("https://api.ipify.org?format=json");
+      return response.data.ip;
+    } catch (error) {
+      console.error("Error fetching IP:", error);
+      throw new Error("Failed to fetch IP address");
+    }
   }
 }
