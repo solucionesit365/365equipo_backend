521688ef68b5130d8972fab7e86b1a26bf6d63b0
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-03-28 15:09:18 +0200

    Anuncios comentado
    Por utilizar MongoDB

--- src/anuncios/anuncios.class.ts
+++ src/anuncios/anuncios.class.ts
@@ -1,16 +1,18 @@
 import { AnuncioDto } from "./anuncios.dto";
-import * as schAnuncios from "./anuncios.mongodb";
+// import * as schAnuncios from "./anuncios.mongodb";
 
 class AnunciosClass {
   async getAnuncios(arrayTiendas: number[]) {
-    const arrayAnuncios = await schAnuncios.getAnuncios(arrayTiendas);
+    return false;
+    // const arrayAnuncios = await schAnuncios.getAnuncios(arrayTiendas);
 
-    if (arrayAnuncios.length > 0) return arrayAnuncios;
-    return null;
+    // if (arrayAnuncios.length > 0) return arrayAnuncios;
+    // return null;
   }
 
   async addAnuncio(anuncio: AnuncioDto) {
-    return await schAnuncios.addAnuncio(anuncio);
+    return false;
+    // return await schAnuncios.addAnuncio(anuncio);
   }
 }
 
--- src/anuncios/anuncios.mongodb.ts
+++ src/anuncios/anuncios.mongodb.ts
@@ -1,21 +1,21 @@
-import { conexion } from "../bbdd/mongodb";
-import { AnuncioDto } from "./anuncios.dto";
+// import { conexion } from "../bbdd/mongodb";
+// import { AnuncioDto } from "./anuncios.dto";
 
-export async function getAnuncios(tiendas: number[]) {
-  const db = (await conexion).db("soluciones");
-  const anuncios = db.collection<AnuncioDto>("anuncios");
-  if (tiendas.includes(-1)) {
-    return await anuncios.find().toArray();
-  }
-  return await anuncios.find({ tiendas: { $all: tiendas } }).toArray();
-}
+// export async function getAnuncios(tiendas: number[]) {
+//   const db = (await conexion).db("soluciones");
+//   const anuncios = db.collection<AnuncioDto>("anuncios");
+//   if (tiendas.includes(-1)) {
+//     return await anuncios.find().toArray();
+//   }
+//   return await anuncios.find({ tiendas: { $all: tiendas } }).toArray();
+// }
 
-export async function addAnuncio(anuncio: AnuncioDto) {
-  const db = (await conexion).db("soluciones");
-  const anuncios = db.collection<AnuncioDto>("anuncios");
+// export async function addAnuncio(anuncio: AnuncioDto) {
+//   const db = (await conexion).db("soluciones");
+//   const anuncios = db.collection<AnuncioDto>("anuncios");
 
-  const resInsert = await anuncios.insertOne(anuncio);
+//   const resInsert = await anuncios.insertOne(anuncio);
 
-  if (resInsert.acknowledged) return resInsert.insertedId;
-  return null;
-}
+//   if (resInsert.acknowledged) return resInsert.insertedId;
+//   return null;
+// }
