4d5a07f4b12d785e5e9387a487b5b4add6088288
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-05-16 12:16:54 +0200

    Update notificaciones.class.ts

--- src/notificaciones/notificaciones.class.ts
+++ src/notificaciones/notificaciones.class.ts
@@ -1,10 +1,33 @@
 import { Injectable } from "@nestjs/common";
 import { NotificacionsBbdd } from "./notificaciones.mongodb";
+import axios from "axios";
 
 @Injectable()
 export class Notificaciones {
   constructor(private readonly schNotificaciones: NotificacionsBbdd) {}
   async saveToken(uid: string, token: string) {
     return await this.schNotificaciones.saveToken(uid, token);
+  }
+
+  async sendMessage(titulo: string, body: string, fcmToken: string) {
+    const resSend = await axios.post(
+      "mi-end-point",
+      {
+        to: fcmToken,
+        notification: {
+          title: titulo,
+          body: body,
+        },
+        data: {
+          exampleKey: "exampleValue",
+        },
+      },
+      {
+        headers: {
+          Authorization: `key=${process.env.FCM_SECRET}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
   }
 }
