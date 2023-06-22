cf0b7697f8d421b5cb78f5721fd8a5688c6388c2
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-06-22 14:01:16 +0200

    Update app.ts

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase-admin/app";
 
-if (process.env.NODE_ENV === "development") {
-  process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
-}
+// if (process.env.NODE_ENV === "development") {
+//   process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
+// }
+// Esto rompe el auth en modo develoment
 
 export const app = initializeApp();
