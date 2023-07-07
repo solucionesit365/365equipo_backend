ea45a35aa0eca8dda6a558c97135187eadc14681
Miguel <mike95anthony@gmail.com>
2023-07-07 17:29:38 +0200

    interface actualizado

--- src/auditorias/auditorias.interface.ts
+++ src/auditorias/auditorias.interface.ts
@@ -6,7 +6,27 @@ export interface AuditoriasInterface {
     descripcion: string;
     caducidad: Date;
     tienda: number;
+    configuracion: {
+        preguntasDependientaA: boolean,
+        preguntasDependientaB_C: boolean,
+        preguntasResponsable: boolean
+    }
     preguntas: {
+        archivo: boolean;
+        pregunta: string;
+        tipo: string;
+    }[];
+    preguntasDependientaA: {
+        archivo: boolean;
+        pregunta: string;
+        tipo: string;
+    }[];
+    preguntasDependientaB_C: {
+        archivo: boolean;
+        pregunta: string;
+        tipo: string;
+    }[];
+    preguntasResponsable: {
         archivo: boolean;
         pregunta: string;
         tipo: string;
