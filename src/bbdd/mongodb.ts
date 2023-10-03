bd464326df33808a90fbf81cfaa47024cb40fcee
Miguel <47475010+MCortez95AC@users.noreply.github.com>
2023-10-03 08:22:19 +0200

    Backend de julio 19

--- src/bbdd/mongodb.ts
+++ src/bbdd/mongodb.ts
@@ -7,7 +7,7 @@ export class MongoDbService {
   private conexion: Promise<MongoClient>;
 
   constructor(private configService: ConfigService) {
-    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@soluciones-365equipo.vbpm2wm.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@365-equipo-new.kfiby.mongodb.net/?retryWrites=true&w=majority`;
     const client = new MongoClient(uri);
     this.conexion = client.connect();
   }
