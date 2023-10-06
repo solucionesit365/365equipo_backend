3aa8100410d2b3706186d057bcc5bcde2fccafe2
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-10-06 12:24:22 +0200

    Update mongodb.ts

--- src/bbdd/mongodb.ts
+++ src/bbdd/mongodb.ts
@@ -7,7 +7,11 @@ export class MongoDbService {
   private conexion: Promise<MongoClient>;
   //Nueva direccion de base de datos
   constructor(private configService: ConfigService) {
-    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@365-equipo-new.kfiby.mongodb.net/?retryWrites=true&w=majority`;
+    let mongoHost: string = null;
+    if (process.env.ENTORNO === "test") {
+      mongoHost = "test-365equipo.cgnccs9.mongodb.net";
+    } else mongoHost = "365-equipo-new.kfiby.mongodb.net";
+    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${mongoHost}`;
     const client = new MongoClient(uri);
     this.conexion = client.connect();
   }
