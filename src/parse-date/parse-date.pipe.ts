d757a106a442088a7df8d156d00a099bddbce5c2
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2023-12-12 15:50:23 +0100

    Update parse-date.pipe.ts

--- src/parse-date/parse-date.pipe.ts
+++ src/parse-date/parse-date.pipe.ts
@@ -9,8 +9,8 @@ import { DateTime } from "luxon";
 @Injectable()
 export class ParseDatePipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata): Date {
-    const luxonDate = DateTime.fromISO("2023-05-15T04:03:00.000+00:00");
-    console.log("luxon date es: ", luxonDate.isValid);
+    const luxonDate = DateTime.fromISO(value);
+
     if (luxonDate.isValid === false) {
       throw new BadRequestException("Fecha inválida.");
     }
