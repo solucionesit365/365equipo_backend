bb5a654cf0c300bc32693285374a26bcd59b835f
Ezequiel <ezequielucho@gmail.com>
2023-12-19 15:11:24 +0100

    Migración 100%

---
+++ prisma/migrations/20231219114021_id_empresa_eliminado/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `idEmpresa` on the `trabajador` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `trabajador` DROP COLUMN `idEmpresa`;
