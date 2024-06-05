a267dc371e5038bebe9a20d3ec11c085c852ce70
ismaelIt365 <132374212+ismaelIt365@users.noreply.github.com>
2024-06-05 16:59:00 +0200

    backend en principio terminado

---
+++ src/videos-formacion/videos-formacion.controller.ts
@@ -0,0 +1,154 @@
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Headers,
+  Body,
+  Get,
+  Query,
+} from "@nestjs/common";
+import { AuthGuard } from "../guards/auth.guard";
+import { videosFormacion365Class } from "./videos-formacion.class";
+import {
+  videosFormacion365Interface,
+  videosVistosFormacion365Interface,
+} from "./videos-formacion.interface";
+
+@Controller("videos-formacion")
+export class VideosFormacionController {
+  constructor(private readonly formacionInstance: videosFormacion365Class) {}
+
+  @UseGuards(AuthGuard)
+  @Post("nuevoVideoFormacion")
+  async nuevoVideoFormacion(@Body() video: videosFormacion365Interface) {
+    try {
+      video.creacion = new Date();
+      return {
+        ok: true,
+        data: await this.formacionInstance.nuevoVideo(video),
+      };
+    } catch (error) {
+      console.log(error);
+      return { ok: false, messag: error.message };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post("nuevoVistoVideoFormacion")
+  async nuevoVistoVideoFormacion(
+    @Body() video: videosVistosFormacion365Interface,
+  ) {
+    try {
+      // Verificar si el video ya ha sido registrado
+      const videoExistente = await this.formacionInstance.findVideoByIdVideo(
+        video.nombre,
+        video.idVideo,
+      );
+      if (videoExistente) {
+        return {
+          ok: false,
+        };
+      }
+      video.visto = new Date();
+      return {
+        ok: true,
+        data: await this.formacionInstance.nuevoVistoVideo(video),
+      };
+    } catch (error) {
+      return { ok: false, messag: error.message };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("getVideosFormacion")
+  async getVideosFormacion() {
+    try {
+      const response = await this.formacionInstance.getVideos();
+
+      if (response) {
+        return {
+          ok: true,
+          data: response,
+        };
+      } else throw Error("No se ha encontrado ningun video de formacion");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post("updateVideoFormacion")
+  async updateVideoFormacion(
+    @Body() videoModificado: videosFormacion365Interface,
+  ) {
+    try {
+      if (await this.formacionInstance.updateVideo(videoModificado))
+        return {
+          ok: true,
+        };
+      throw Error("No se ha podido modificar el video de formacion");
+    } catch (err) {
+      console.log(err);
+      return { ok: false, message: err.message };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post("deleteVideoFormacion")
+  async deleteVideoFormacion(@Body() { _id }: { _id: string }) {
+    try {
+      if (await this.formacionInstance.deleteVideo(_id))
+        return {
+          ok: true,
+        };
+      throw Error("No se ha podido borrar el video de formacion");
+    } catch (err) {
+      console.log(err);
+      return { ok: false, message: err.message };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post("contadorViews")
+  async incrementarContadorViews(@Query("videoId") videoId: string) {
+    try {
+      const respvideo = await this.formacionInstance.incrementarContadorViews(
+        videoId,
+      );
+
+      return { mensaje: "Contador incrementado con éxito", respvideo };
+    } catch (error) {
+      return { mensaje: "Error al incrementar el contador de vistas", error };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("views")
+  async views() {
+    try {
+      const respvideo = await this.formacionInstance.views();
+
+      return { mensaje: "se muestran los views", respvideo };
+    } catch (error) {
+      return { mensaje: "Error al mostrar los views", error };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Get("verificarVideoVisto")
+  async verificarVideoVisto(
+    @Query("nombre") nombre: string,
+    @Query("idVideo") idVideo: string,
+  ) {
+    try {
+      const videoExistente = await this.formacionInstance.findVideoByIdVideo(
+        nombre,
+        idVideo,
+      );
+      return { existe: !!videoExistente };
+    } catch (error) {
+      console.log(error);
+      return { existe: false, message: error.message };
+    }
+  }
+}
---
+++ src/videos-formacion/videos-formacion.mongodb.ts
@@ -0,0 +1,115 @@
+import { Injectable } from "@nestjs/common";
+import { ObjectId } from "mongodb";
+import {
+  videosFormacion365Interface,
+  videosVistosFormacion365Interface,
+} from "./videos-formacion.interface";
+import { MongoService } from "../mongo/mongo.service";
+
+@Injectable()
+export class videosFormacion365Mongo {
+  constructor(private readonly mongoDbService: MongoService) {}
+
+  async nuevoVideo(video: videosFormacion365Interface) {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect =
+      db.collection<videosFormacion365Interface>("videosFormacion");
+
+    const resInsert = await videosCollect.insertOne(video);
+
+    if (resInsert.acknowledged) return resInsert.insertedId;
+
+    throw Error("No se ha podido crear el video de formacion");
+  }
+
+  async nuevoVistoVideo(videos: videosVistosFormacion365Interface) {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect = db.collection<videosVistosFormacion365Interface>(
+      "videosVistosFormacion",
+    );
+
+    const resInsert = await videosCollect.insertOne(videos);
+
+    if (resInsert.acknowledged) return resInsert.insertedId;
+
+    throw Error("No se ha podido crear el video de formacion");
+  }
+
+  async getVideos() {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect =
+      db.collection<videosFormacion365Interface>("videosFormacion");
+    const response = await videosCollect.find({}).toArray();
+
+    return response;
+  }
+
+  async findVideoByIdVideo(nombre: string, idVideo: string) {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect = db.collection<videosVistosFormacion365Interface>(
+      "videosVistosFormacion",
+    );
+
+    return await videosCollect.findOne({ nombre: nombre, idVideo: idVideo });
+  }
+
+  async updatevideo(video: videosFormacion365Interface) {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect =
+      db.collection<videosFormacion365Interface>("videosFormacion");
+
+    const resUpdate = await videosCollect.updateOne(
+      {
+        _id: new ObjectId(video._id),
+      },
+      {
+        $set: {
+          titulo: video.titulo,
+          descripcion: video.descripcion,
+          urlVideo: video.urlVideo,
+        },
+      },
+    );
+
+    return resUpdate.acknowledged;
+  }
+
+  async deleteVideo(_id: string) {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect =
+      db.collection<videosFormacion365Interface>("videosFormacion");
+
+    const resDelete = await videosCollect.deleteOne({
+      _id: new ObjectId(_id),
+    });
+
+    return resDelete.acknowledged && resDelete.deletedCount > 0;
+  }
+
+  async incrementarContadorViews(videoId: string) {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect =
+      db.collection<videosFormacion365Interface>("videosFormacion");
+
+    const resUpdate = await videosCollect.updateOne(
+      { _id: new ObjectId(videoId) },
+      { $inc: { views: 1 } },
+    );
+
+    if (!resUpdate.modifiedCount) {
+      throw Error("No se pudo incrementar el contador de vistas del video");
+    }
+  }
+
+  async views() {
+    const db = (await this.mongoDbService.getConexion()).db("soluciones");
+    const videosCollect =
+      db.collection<videosFormacion365Interface>("videosFormacion");
+
+    const vistasPorVideo = await videosCollect
+      .aggregate([{ $project: { _id: 1, views: 1, titulo: 1 } }])
+      .toArray();
+
+    return vistasPorVideo;
+  }
+}
