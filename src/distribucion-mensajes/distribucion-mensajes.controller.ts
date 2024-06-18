bd799168f06894068c97cc18da8ca95d78b784eb
ismaelIt365 <132374212+ismaelIt365@users.noreply.github.com>
2024-06-18 11:19:41 +0200

    FXPD-24 borrar de distribucion mensaje terminado

--- src/distribucion-mensajes/distribucion-mensajes.controller.ts
+++ src/distribucion-mensajes/distribucion-mensajes.controller.ts
@@ -94,6 +94,8 @@ export class DistribucionMensajesController {
   @Post("deleteMessage")
   async deleteMessage(@Query() { id }) {
     try {
+      console.log(id);
+      
       const deleteMensaje = await this.DistribucionMensajesClass.deleteMessage(
         id,
       );
