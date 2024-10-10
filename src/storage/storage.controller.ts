afe86439a8c5547c86342fc0b34b409821882b15
Ezequiel <ezequielucho@gmail.com>
2024-10-10 15:57:24 +0200

    Poc a poc

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  Body,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { StorageService } from "./storage.service";
+import { UploadFileDto } from "./storage.dto";
+import { Express } from "express"; // Importa Express para usar el tipo Multer
 
-@Controller('storage')
-export class StorageController {}
+@Controller("storage")
+export class StorageController {
+  constructor(private readonly storageService: StorageService) {}
+
+  @Post("uploadFile")
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadFile(
+    @Body() uploadFileDto: UploadFileDto,
+    @UploadedFile() file: Express.Multer.File, // Ajuste aquí
+  ) {
+    const fileBuffer = file.buffer;
+    const { filePath, contentType } = uploadFileDto;
+
+    try {
+      const url = await this.storageService.uploadFile(
+        filePath,
+        fileBuffer,
+        contentType,
+      );
+      return { url };
+    } catch (error) {
+      console.error("Error uploading file", error);
+      throw error;
+    }
+  }
+}
