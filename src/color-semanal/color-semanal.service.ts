93f953e46ae14d30d117800777f56cf74550efab
Ezequiel <ezequielucho@gmail.com>
2024-11-13 15:32:05 +0100

    Color Semanal cambiado

--- src/color-semanal/color-semanal.service.ts
+++ src/color-semanal/color-semanal.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { Color } from "./color-semanal.dto";
 import { $Enums } from "@prisma/client";
+import { DateTime } from "luxon";
 
 @Injectable()
 export class ColorSemanalService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async saveColorOut(color: Color) {
+  async saveColorIn(color: Color) {
     return await this.prisma.color.update({
       where: {
-        id: "SALE_COLOR",
+        id: "ENTRA_COLOR",
       },
       data: {
         value: color,
@@ -19,17 +20,20 @@ export class ColorSemanalService {
   }
 
   async getColors() {
-    const colorOut = (
-      await this.prisma.color.findUnique({
-        where: {
-          id: "SALE_COLOR",
-        },
-      })
-    ).value;
+    const colorData = await this.prisma.color.findUnique({
+      where: {
+        id: "ENTRA_COLOR",
+      },
+    });
 
-    const colorIn = this.getColorIn(colorOut);
+    const colorIn = colorData.value;
+    const colorOut = this.getColorOut(colorIn);
 
-    return { colorOut, colorIn };
+    return {
+      colorOut,
+      colorIn,
+      updatedAt: DateTime.fromJSDate(colorData.updatedAt).toISO(),
+    };
   }
 
   getColorIn(colorOut: $Enums.ColorSemanal) {
@@ -42,6 +46,20 @@ export class ColorSemanalService {
         return "orange";
       case Color.Brown:
         return "blue";
+    }
+  }
+
+  // Nueva para Jesus
+  getColorOut(colorIn: $Enums.ColorSemanal) {
+    switch (colorIn) {
+      case Color.Brown:
+        return "green";
+      case Color.Green:
+        return "orange";
+      case Color.Orange:
+        return "blue";
+      case Color.Blue:
+        return "brown";
     }
   }
 }
