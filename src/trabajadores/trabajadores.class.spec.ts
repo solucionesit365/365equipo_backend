32e3c496f2318643868beed6fdf7656cde940e60
eze365obrador <ezequielcarissimo@grupohorreols.com>
2025-05-07 14:36:02 +0200

    Ejemplo test

--- src/trabajadores/trabajadores.class.spec.ts
+++ src/trabajadores/trabajadores.class.spec.ts
@@ -1,5 +1,29 @@
 describe("TrabajadorService", () => {
   it("should be defined", () => {
+    expect(1).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(2).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(3).toBeDefined();
+  });
+  it("should be defined", () => {
     expect(4).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(5).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(6).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(7).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(8).toBeDefined();
+  });
+  it("should be defined", () => {
+    expect(9).toBeDefined();
   });
 });
