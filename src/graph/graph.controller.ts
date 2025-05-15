cc286bd1223183022aecab1440a55d24ba3ceeb9
Ismael <imariduena@365obrador.com>
2025-05-15 16:13:51 +0200

    Update graph.controller.ts

--- src/graph/graph.controller.ts
+++ src/graph/graph.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post, Get, Query } from "@nestjs/common";
+import { Controller, Post, Get, Query, UseGuards } from "@nestjs/common";
 import { GraphService } from "./graph.service";
+// import { DateTime } from "luxon";
+import { AuthGuard } from "src/guards/auth.guard";
 @Controller("graph")
 export class GraphController {
   constructor(private readonly graphService: GraphService) {}
@@ -14,12 +16,12 @@ export class GraphController {
     return this.graphService.getRooms();
   }
 
+  @UseGuards(AuthGuard)
   @Get("checkDisponibilidadRoom")
   async checkDisponibilidadRoom(@Query() { roomEmail, startDate, endDate }) {
     console.log(startDate);
     console.log(endDate);
 
-    // const endDate = DateTime.fromFormat("2025-05-12 23:00", "yyyy-MM-dd HH:mm");
     return this.graphService.getRoomAvailability(roomEmail, startDate, endDate);
   }
 }
