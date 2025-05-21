826f2367775984992a6cb48d6154b685fd10c267
eze365obrador <ezequielcarissimo@grupohorreols.com>
2025-05-21 14:12:19 +0200

    Update auth.guard.ts

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -38,6 +38,7 @@ export class AuthGuard implements CanActivate {
       });
       request.user = usuarioCompleto;
     } catch (err) {
+      console.log(err);
       throw new UnauthorizedException("No estás autorizado/a");
     }
     return true;
