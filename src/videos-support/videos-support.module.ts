45b3649914da44df9fb92cf769960106ca34febf
MaraGarcia06 <maragarcia@grupohorreols.com>
2025-11-10 12:57:09 +0100

    borrar service

--- src/videos-support/videos-support.module.ts
+++ src/videos-support/videos-support.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
-import { VideosSupportService } from './videos-support.service';
 import { VideosSupportController } from "./videos-support.controller";
 
 @Module({
   controllers: [VideosSupportController],
-  providers: [VideosSupportService],
 })
 export class VideosSupportModule {}
