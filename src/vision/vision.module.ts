ff7dfa8d7b6180941685eb113b385442f2a08340
eze365obrador <ezequielcarissimo@grupohorreols.com>
2025-11-17 11:11:33 +0100

    Clean unused modules (#311)
    Co-authored-by: dobleamarilla <ezequielucho@gmail.com>

--- src/vision/vision.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { VisionService } from "./vision.service";
-import { VisionController } from "./vision.controller";
-
-@Module({
-  providers: [VisionService],
-  controllers: [VisionController],
-})
-export class VisionModule {}
