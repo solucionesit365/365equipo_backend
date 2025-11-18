8fa4d7020ae91bffde9aad0890a371eddf487f0b
dobleamarilla <ezequielucho@gmail.com>
2025-11-18 16:35:00 +0100

    Video stream

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -3,10 +3,11 @@ import { VideosController } from "./videos.controller";
 import { VideosService } from "./videos.service";
 import { StorageModule } from "../storage/storage.module";
 import { CryptoModule } from "../crypto/crypto.module";
+import { VideoStreamAuthGuard } from "../guards/video-stream-auth.guard";
 
 @Module({
   imports: [StorageModule, CryptoModule],
   controllers: [VideosController],
-  providers: [VideosService],
+  providers: [VideosService, VideoStreamAuthGuard],
 })
 export class VideosModule {}
